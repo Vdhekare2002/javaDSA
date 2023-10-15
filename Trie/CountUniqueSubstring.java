1dd51a7107b9c476585eda2a806a62d57e0fe218
Vaishnavi Dhekare <118724788+Vdhekare2002@users.noreply.github.com>
2023-10-15 20:04:10 +0530

    Add files via upload

---
+++ Trie/CountUniqueSubstring.java
@@ -0,0 +1,7 @@
+package Trie;
+//Given a String of length n of lowercase alphabet characters, we need to count total number of distinct substrings of this String
+public class CountUniqueSubstring {
+  public static void main (String[]args) {
+	  
+  }
+}
---
+++ Trie/Prefix.java
@@ -0,0 +1,10 @@
+package Trie;
+
+public class Prefix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
